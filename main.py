de2cdd0a39231a26364aad7ae237dae5bbbc2280
Pragadeesh Raj <pragadeesh3000@gmail.com>
2020-10-24 18:40:50 -0700

    Playground Commit

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Hell9o World")
+def mean(List):
+    total = 0
+    for x in List:
+        total += x
+    mean = total / len(List)
+    return mean
+
+Number_List = []
+
+while(True):
+    ask = input('enter a number and say "stop" to end: ')
+
+    if ask == 'stop':
+        break
+    Number_List.append(int(ask))
+
+mean = str(mean(Number_List))
+
+print("mean:"+mean)
